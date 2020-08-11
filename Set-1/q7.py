923c1b09a7f68c35b3dd146d4354a9e238c5c7e3
preethamk2000 <preethamk18042000@gmail.com>
2020-08-12 02:25:58 +0530

    OOP to Set-1 ques-7

--- Set-1/q7.py
+++ Set-1/q7.py
@@ -2,17 +2,33 @@ from cryptography.hazmat.primitives.ciphers import Cipher,algorithms,modes
 from cryptography.hazmat.backends import default_backend
 import base64
 
-backend = default_backend()
+class AES_ECB:
 
-key = b"YELLOW SUBMARINE"
-cipher = Cipher(algorithm=algorithms.AES(key),mode=modes.ECB(),backend=backend)
+    backend = default_backend()
 
-decryptor = cipher.decryptor()
+    def __init__(self,key):
+        self.cipher = Cipher(algorithm=algorithms.AES(key),mode=modes.ECB(),backend=self.backend)
+    
+    def encrypt(self,plaintext):
+        encryptor = self.cipher.encryptor()
+        ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+        return ciphertext
+    
+    def decrypt(self,ciphertext):
+        decryptor = self.cipher.decryptor()
+        plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+        return plaintext
 
-plaintext = b""
 
-# Here open mode can be 'r' or even 'rb'
-with open('7.txt','rb') as file:
-    plaintext += decryptor.update(base64.b64decode(file.read())) + decryptor.finalize()
+if __name__=="__main__":
+    
+    key = b"YELLOW SUBMARINE"
+    plaintext = ""
 
-print(plaintext.decode('utf-8'))
+    enc_scheme = AES_ECB(key)
+
+    with open('7.txt','rb') as file:
+        ciphertext = base64.b64decode(file.read())
+        plaintext = enc_scheme.decrypt(ciphertext)
+
+    print(plaintext.decode('utf-8'))
