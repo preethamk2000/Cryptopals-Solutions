e97ee6ace924ebe39cc4060dd2b14c4fe4a79a79
preethamk2000 <preethamk18042000@gmail.com>
2020-08-16 01:59:13 +0530

    [FIX] Set-2 ques-9 pad and unpad functions now operate on bytes & not strings

--- Set-2/q9.py
+++ Set-2/q9.py
@@ -1,46 +1,47 @@
 from string import printable
-import io
 
+# KEY POINT LEARNT: WHEN APPENDING HEX STRINGS LIKE \X01 ETC. OPERATE IN BYTES AND NOT IN STRINGS
+# BECAUSE IN STRINGS IT GETS COMPLICATED DUE TO THE FACT THAT UNNECESSARY '\' CHARACTERS GET APPENDED TOGETHER
+# IE '\X01' BECOMES '\\X01' AND ITS SO CALLED HEX-I-NESS GETS LOST :P
+
+# Remove unnecessary elements from printable characters
 printable_refined = printable[0:len(printable)-5]
 
+# Accepts a string and returns padded plaintext as bytearray
 def pkcs7_padding(plaintext,blocksize=8):
-    padded_text = "" + plaintext
-    padding_length = blocksize-len(plaintext)%blocksize
-    padding_string = "\\x{:02x}".format(padding_length)
-    for i in range(padding_length):
-        padded_text += padding_string
+    padded_text = plaintext.encode('ascii')
+    if len(plaintext)%blocksize != 0:
+        padding_length = blocksize-len(plaintext)%blocksize
+        padding_string = "{:02x}".format(padding_length)
+        for i in range(padding_length):
+            padded_text += bytes.fromhex(padding_string)
     return padded_text
 
 def pkcs7_padding_remove(padded_text,blocksize=8):
-    # FUCKING PYTHON ADDS ANOTHER '\' CHARACTER EVEN FOR \X?? TYPE HEX DIGITS
-    # lastchr = padded_text[::-1][0]
-    # So instead get the last two hex digits and use them to un pad 
-    lastchr = padded_text[len(padded_text)-2:len(padded_text)]
-    lastchr_int = int(lastchr,16)
-    # Here Im just checking if the last hex is a printable character and if it is then it must be a block with a multiple of keysize
+    # Since it is in bytes, the individual elements are already in INT
+    lastchr_int = padded_text[::-1][0]
+    # Here Im just checking if the last hex is a printable character and if it is then it must be a block with a multiple of keysize since even with keysize of 32 bytes all the characters do not have meaning when printable
     if(printable_refined.find( chr(lastchr_int) )!=-1):
         return padded_text
     else:
         # padding_length = int.from_bytes(padded_text[::-1][0].encode(),"little")
         # plaintext = padded_text[0:len(padded_text)-padding_length]
         # OR FOR SIMPLER SHIT
-        # This 4 is added
-        plaintext = padded_text[0: -4*lastchr_int ]
+        plaintext = padded_text[0: -lastchr_int ]
         return plaintext
 
 if __name__=="__main__":
-    # plaintext = "YELLOW SUBMARINE"
-    plaintext = "012345678901234567890123"
-    # plaintext = "YELLOW SUBMARINEYELLOW SUBMA"
-    blocksize = 32
-    if len(plaintext)%blocksize != 0:
-        padded_text = pkcs7_padding(plaintext,blocksize)
-    else:
-        padded_text = plaintext
-    print("Plaintext ->" + plaintext + "<- padded to " + str(blocksize) + " blocks is :" + padded_text + "--------END")
+    plaintext = "YELLOW SUBMARINE"
+    # plaintext = "012345678901234567890123"
+    # plaintext = "YELLOW SUBMARINEYELLOW SUBMARINEYELLOW SU"
+    blocksize = 20
 
-    # padded_text = "12345678"
+    padded_text = pkcs7_padding(plaintext,blocksize)
+    print("\nPlaintext ->" + plaintext + "<- padded to blocksize of " + str(blocksize) + " is :")
+    print(padded_text)
 
-    print("----------Removing padding----------------")
+    print("\n----------Removing padding----------------")
     original_plaintext = pkcs7_padding_remove(padded_text,blocksize)
-    print("Original text: " + original_plaintext)
+    print("Original text: ",original_plaintext)
+
+    print(len(padded_text))
